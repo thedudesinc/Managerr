0f8475c95475797fc9a9f2d2e95e56a37d0a7b91
Jomack16 <jejones92@yahoo.com>
2022-11-02 16:52:09 -0500

    added support files

---
+++ RunPlexDiscordBot.bat
@@ -0,0 +1,3 @@
+set log="C:\Users\user\Documents\DiscordBots\PlexVM1-B\RunPlexDiscordBot.log"
+echo "started PlexVM1 bot at this time | %date% - %time%" >> %log%
+python C:\Users\user\Documents\DiscordBots\PlexVM1-B\PlexDiscordBot.py
