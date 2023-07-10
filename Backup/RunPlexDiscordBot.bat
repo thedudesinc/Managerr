07061aac7b003d2eaea314807fb2619c0aee2af7
Jomack16 <jejones92@yahoo.com>
2023-07-10 00:27:50 -0500

    removed old unnecessary folder.

--- Backup/RunPlexDiscordBot.bat
+++
@@ -1,3 +0,0 @@
-set log="C:\Users\user\Documents\DiscordBots\PlexVM1-B\RunPlexDiscordBot.log"
-echo "started PlexVM1 bot at this time | %date% - %time%" >> %log%
-python C:\Users\user\Documents\DiscordBots\PlexVM1-B\PlexDiscordBot.py
